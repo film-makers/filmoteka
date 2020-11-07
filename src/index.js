032b6afdc50dda6dec1822db5f45e4970d8f489b
Mac_Sizy <polzovatel@MacBook-Pro-Polzovatel.local>
2020-11-07 16:21:14 +0200

    Pagination 0.002

--- src/index.js
+++ src/index.js
@@ -3,12 +3,10 @@ import axios from 'axios';
 import refs from './js/refs';
 import filmCard from './templates/film-card.hbs';
 import genres from './modules/genres';
-//import { renderSync } from 'node-sass';
 
 const API_KEY = 'bb0a149304db2d054e912403b986db46';
 
 axios.defaults.baseURL = 'https://api.themoviedb.org/3';
-//axios.defaults.headers.common['Access-Control-Allow-Origin'] = API_KEY;
 
 const movies = axios.get(`/trending/movies/week?api_key=${API_KEY}`);
 
@@ -20,25 +18,18 @@ movies.then(({ data: { results } }) => {
       : 'No data',
   }));
 
-  console.log(mappedResults);
-
   const finalResults = mappedResults.map(item => ({
     ...item,
     genre_ids: item.genre_ids
       .map(number => {
-        // console.log(number);
         const genre = genres.find(genre => genre.id === number);
-        console.log(genre);
 
         return genre?.name;
       })
       .join(', '),
   }));
 
-  console.log(finalResults);
-
   const cards = filmCard(finalResults);
-  console.log(finalResults);
 
   refs.container.insertAdjacentHTML('beforeend', cards);
 });
